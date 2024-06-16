bb6a310296f5c60014115c87d9bb98ec6863d094
rishitha <rishithareddy062@gmail.com>
2024-06-16 22:53:12 +0530

    backend

---
+++ backend/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://your_server_ip:5000/api',
+});
+
+export const register = async (email, password) => {
+  return await api.post('/users/register', { email, password });
+};
+
+export const login = async (email, password) => {
+  return await api.post('/users/login', { email, password });
+};
+
+export const getGrades = async () => {
+  return await api.get('/grades');
+};
+
+export const getAnnouncements = async () => {
+  return await api.get('/announcements');
+};
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = 'your_mongodb_connection_string';
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/grades', require('./routes/grades'));
+app.use('/api/announcements', require('./routes/announcements'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
