5d0fdb9b01026e0716e74cb002fcbd6d468bc914
rishitha <rishithareddy062@gmail.com>
2024-06-17 17:19:18 +0530

    test

--- api.js
+++ api.js
@@ -28,6 +28,21 @@ const api = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
 
+
+
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    console.log(response.data);
+  } catch (error) {
+    console.error('There was an error making the request:', error);
+  }
+};
+
+fetchData();
+
+
 export const register = async (email, password) => {
   return await api.post('/users/register', { email, password });
 };
